7c730ac2f4bb505f4dddaf7f208e26c05722c8b3
jiangdaa <jiangdaa@qq.com>
2017-12-07 22:10:59 +0800

    添加了。。

---
+++ darg.js
@@ -0,0 +1,19 @@
+function darg(id) {
+    var el = document.getElementById(id)
+    var disX = 0;
+    var disY = 0;
+    el.onmousedown = function (ev) {
+        disX = ev.clientX - el.offsetLeft;
+        disY = ev.clientY - el.offsetTop;
+
+        document.onmousemove = function (ev) {
+            el.style.left = ev.clientX - disX + 'px';
+            el.style.top = ev.clientY - disY + 'px';
+        }
+
+        document.onmouseup = function () {
+            document.onmousemove = null;
+            document.onmouseup = null;
+        }
+    }
+}
